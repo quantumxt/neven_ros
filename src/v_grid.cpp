93cb2483e28deaf89d02f92382a73660b27b9f61
Photon <1487quantum@users.noreply.github.com>
2020-09-01 19:00:28 +0800

    Group as functions

--- src/v_grid.cpp
+++ src/v_grid.cpp
@@ -21,17 +21,18 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
 
-ros::Publisher pub;
+ros::Publisher pubObjSeg;
 ros::Publisher pubMarker;
 
-visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const float& pt_z, const float& idx)
+visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const float& pt_z, const float& idx, const char * mkr_ns, const int& mType)
 {
+
     visualization_msgs::Marker marker;
     marker.header.frame_id = "cam_link";
     marker.header.stamp = ros::Time();
-    marker.ns = "pcl_marker_centroid";
+    marker.ns = mkr_ns;
     marker.id = idx;
-    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.type = mType;
     marker.action = visualization_msgs::Marker::ADD;
     marker.pose.position.x = pt_x;
     marker.pose.position.y = pt_y;
@@ -51,52 +52,46 @@ visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const
     return marker;
 }
 
+visualization_msgs::Marker addCentroid(const float& pt_x, const float& pt_y, const float& pt_z, const float& idx)
+{
+    return addMarker(pt_x, pt_y, pt_z, idx, "pcl_marker_centroid", visualization_msgs::Marker::SPHERE);
+}
+
 //Downsample/Reduces num of pcl
-void vx_grid(const pcl::PointCloud<pcl::PointXYZRGBA> &cld_in, pcl::PointCloud<pcl::PointXYZRGBA> &cld_out){
-//    ROS_INFO("Running Voxel Filter...");
- constexpr float leafSize{ 0.005f }; //Set it to 0.5cm
-constexpr float f_limit[2] = {0.01f, 1.8f};
+void vx_grid(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const float &leafSize)
+{
+    //    ROS_INFO("Running Voxel Filter...");
+    constexpr float f_limit[2] = { 0.01f, 1.8f };
     pcl::VoxelGrid<pcl::PointXYZRGBA> vxg;
 
     vxg.setInputCloud(cld_in.makeShared());
     vxg.setLeafSize(leafSize, leafSize, leafSize);
-    vxg.setFilterLimits(f_limit[0],f_limit[1]);
+    vxg.setFilterLimits(f_limit[0], f_limit[1]);
     vxg.setFilterFieldName("z");
     vxg.setFilterLimitsNegative(false);
     vxg.filter(cld_out);
-    pcl::io::savePCDFileASCII("save_1.pcd", cld_out);
+    pcl::io::savePCDFileASCII("pcl_vxg.pcd", cld_out);
 }
 
-void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& pcl_in)
+//Filters noise
+void sor_filter(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const int &mean_k, const float &std_thres)
 {
-    // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
-    pcl::PointCloud<pcl::PointXYZRGBA> cloud;
-    pcl::PointCloud<pcl::PointXYZRGBA> cloud_filtered, cloud_filtered2, cloud_filtered3, cloud_tmp;
-    pcl::fromROSMsg(*pcl_in, cloud);
-
-    ROS_INFO("Running CB...");
-
-    // setting alpha = 1.0
-    for (size_t i = 0; i < cloud.points.size(); ++i) {
-        cloud.points[i].a = 255;
-    }
-
-   
-
-vx_grid(cloud,cloud_filtered);  //Voxel filter
-   
-
-    //StatisticalOutlierRemoval Filter
     pcl::StatisticalOutlierRemoval<pcl::PointXYZRGBA> sor;
-    sor.setInputCloud(cloud_filtered.makeShared());
-    sor.setMeanK(50);
-    sor.setStddevMulThresh(0.15);
-    sor.filter(cloud_filtered2);
-    pcl::io::savePCDFileASCII("save_2.pcd", cloud_filtered2);
 
+    sor.setInputCloud(cld_in.makeShared());
+    sor.setMeanK(mean_k);
+    sor.setStddevMulThresh(std_thres);
+    sor.filter(cld_out);
+    pcl::io::savePCDFileASCII("pcl_sor.pcd", cld_out);
+}
+
+void p_seg(pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out)
+{
     //Segmentation
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+
+    pcl::PointCloud<pcl::PointXYZRGBA> cloud_tmp; //Temp pcl
 
     pcl::SACSegmentation<pcl::PointXYZRGBA> seg;
 
@@ -107,36 +102,52 @@ vx_grid(cloud,cloud_filtered);  //Voxel filter
     seg.setMaxIterations(1000);
     seg.setDistanceThreshold(0.01);
 
-    int i = 0, nr_points = (int)cloud_filtered2.size();
-    /*
-//Planar segmentation
-  // While 30% of the original cloud is still there
-  while (cloud_filtered2.size () > 0.3 * nr_points)
-  {
-    // Segment the largest planar component from the remaining cloud
-    seg.setInputCloud(cloud_filtered2.makeShared());
-    seg.segment(*inliers, *coefficients);
-    if (inliers->indices.size () == 0)
-    {
-      ROS_INFO("Could not estimate a planar model for the given dataset.");
-      break;
+    int i = 0, nr_points = (int)cld_in.size();
+
+    //Planar segmentation
+    // While 30% of the original cloud is still there
+    while (cld_in.size() > 0.3 * nr_points) {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud(cld_in.makeShared());
+        seg.segment(*inliers, *coefficients);
+        if (inliers->indices.size() == 0) {
+            ROS_INFO("Could not estimate a planar model for the given dataset.");
+            break;
+        }
+
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZRGBA> extract;
+        extract.setInputCloud(cld_in.makeShared());
+        extract.setIndices(inliers);
+        extract.setNegative(false);
+        extract.filter(cld_out);
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative(true);
+        extract.filter(cloud_tmp);
+        cld_in.swap(cloud_tmp);
+
+        i++;
+    }
+}
+
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& pcl_in)
+{
+    // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+    pcl::PointCloud<pcl::PointXYZRGBA> cloud;
+    pcl::PointCloud<pcl::PointXYZRGBA> cloud_filtered, cloud_filtered2, cloud_filtered3;
+    pcl::fromROSMsg(*pcl_in, cloud);
+
+    //ROS_INFO("Running CB...");
+
+    // setting alpha = 1.0
+    for (size_t i = 0; i < cloud.points.size(); ++i) {
+        cloud.points[i].a = 255;
     }
 
-    // Extract the planar inliers from the input cloud
-  pcl::ExtractIndices<pcl::PointXYZRGBA> extract;
-    extract.setInputCloud(cloud_filtered2.makeShared());
-    extract.setIndices(inliers);
-    extract.setNegative(false);
-    extract.filter(cloud_filtered3);
-
-    // Remove the planar inliers, extract the rest
-    extract.setNegative (true);
-    extract.filter(cloud_tmp);
-    cloud_filtered2.swap (cloud_tmp);
-
-    i++;
-  }
-*/
+    vx_grid(cloud, cloud_filtered,  0.005f); 		 //Voxel filter, 0.5cm leafsize
+    sor_filter(cloud_filtered, cloud_filtered2, 50, 0.15f); //StatisticalOutlierRemoval Filter, mean_k:50, std_dv_threshold: 0.15f
+    //p_seg(cloud_filtered2,cloud_filtered3);	 //Segmentation
 
     // Creating the KdTree object for the search method of the extraction
     pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
@@ -161,9 +172,9 @@ vx_grid(cloud,cloud_filtered);  //Voxel filter
         for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
             cloud_cluster->push_back((cloud_filtered2)[*pit]); //*
             //ROS_INFO("XX [%i]: %i",j, *pit);
-            cloud_filtered.points[*pit].r = 255;
+            cloud_filtered.points[*pit].r = 0;
             cloud_filtered2.points[*pit].g = 40 * (j);
-            cloud_filtered2.points[*pit].b = 255 - 40 * j;
+            cloud_filtered2.points[*pit].b = 0;
             cloud_filtered2.points[*pit].a = 255;
         }
         cloud_cluster->width = cloud_cluster->size();
@@ -176,11 +187,11 @@ vx_grid(cloud,cloud_filtered);  //Voxel filter
         pcl::compute3DCentroid(*cloud_cluster, centroid);
         ROS_INFO("Centroid [%i]: %f %f %f %f", j, centroid[0], centroid[1], centroid[2], centroid[3]);
 
-        m_a.markers.push_back(addMarker(centroid[0], centroid[1], centroid[2], j));
+        m_a.markers.push_back(addCentroid(centroid[0], centroid[1], centroid[2], j));
 
-        pub.publish(cloud_filtered2);
+        pubObjSeg.publish(cloud_filtered2);
 
-        j++;
+        ++j;
     }
     pcl::io::savePCDFileASCII("save_3.pcd", cloud_filtered2);
     pubMarker.publish(m_a);
@@ -194,12 +205,11 @@ int main(int argc, char** argv)
 
     //ros::param::set("dist_th", 0.1);	// Set ROS param
 
-    // Create a ROS subscriber for the input point cloud
-    ros::Subscriber sub = nh.subscribe("/cam/points2", 1, cloud_cb);
+    ros::Subscriber sub = nh.subscribe("/cam/points2", 1, cloud_cb);	// Create a ROS subscriber for the input point cloud
 
     // Create a ROS publisher for the model coefficients
-    pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGBA> >("obj_segmentation", 1);
-    pubMarker = nh.advertise<visualization_msgs::MarkerArray>("obj_marker", 1);
+    pubObjSeg = nh.advertise<pcl::PointCloud<pcl::PointXYZRGBA> >("obj_segmentation", 1);
+    pubMarker = nh.advertise<visualization_msgs::MarkerArray>("obj_centroid", 1);
 
     // Spin
     ros::spin();
