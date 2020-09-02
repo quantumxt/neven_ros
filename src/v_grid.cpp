ea1e1c691ebe31b80de6b95a6db985afaf195228
Photon <1487quantum@users.noreply.github.com>
2020-09-02 13:25:38 +0800

    Add Volume boundary marker
    Represented as a cuboid marker

--- src/v_grid.cpp
+++ src/v_grid.cpp
@@ -21,12 +21,70 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
 
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Vector3.h>
+
 ros::Publisher pubObjSeg;
 ros::Publisher pubMarker;
+ros::Publisher pubBoundRect;
 
-visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const float& pt_z, const float& idx, const char * mkr_ns, const int& mType)
+//Create minMax struct to keep track of pcl cluster boundary
+struct pt {
+    float x;
+    float y;
+    float z;
+};
+
+struct minMax {
+    pt pt_min;
+    pt pt_max;
+};
+
+void initMinMax(minMax& src)
 {
+    constexpr float pMin{ 10000.0f };
+    src.pt_min.x = pMin;
+    src.pt_min.y = pMin;
+    src.pt_min.z = pMin;
+    src.pt_max.x = -pMin;
+    src.pt_max.y = -pMin;
+    src.pt_max.z = -pMin;
+}
 
+void compareVals(const pcl::PointXYZRGBA& pcl_in, minMax& src)
+{
+    src.pt_min.x = pcl_in.x < src.pt_min.x ? pcl_in.x : src.pt_min.x;
+    src.pt_min.y = pcl_in.y < src.pt_min.y ? pcl_in.y : src.pt_min.y;
+    src.pt_min.z = pcl_in.z < src.pt_min.z ? pcl_in.z : src.pt_min.z;
+    src.pt_max.x = pcl_in.x > src.pt_max.x ? pcl_in.x : src.pt_max.x;
+    src.pt_max.y = pcl_in.y > src.pt_max.y ? pcl_in.y : src.pt_max.y;
+    src.pt_max.z = pcl_in.z > src.pt_max.z ? pcl_in.z : src.pt_max.z;
+}
+
+geometry_msgs::Pose createPose(const float& sx, const float& sy, const float& sz)
+{
+    geometry_msgs::Pose ps;
+    ps.position.x = sx;
+    ps.position.y = sy;
+    ps.position.z = sz;
+    ps.orientation.x = 0.0;
+    ps.orientation.y = 0.0;
+    ps.orientation.z = 0.0;
+    ps.orientation.w = 1.0;
+    return ps;
+}
+
+geometry_msgs::Vector3 createScale(const float& sx, const float& sy, const float& sz)
+{
+    geometry_msgs::Vector3 scl;
+    scl.x = sx;
+    scl.y = sy;
+    scl.z = sz;
+    return scl;
+}
+
+visualization_msgs::Marker addMarker(const geometry_msgs::Pose& mkr_pose, const geometry_msgs::Vector3& mkr_size, const float& idx, const char* mkr_ns, const int& mType)
+{
     visualization_msgs::Marker marker;
     marker.header.frame_id = "cam_link";
     marker.header.stamp = ros::Time();
@@ -34,16 +92,8 @@ visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const
     marker.id = idx;
     marker.type = mType;
     marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = pt_x;
-    marker.pose.position.y = pt_y;
-    marker.pose.position.z = pt_z;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = 0.05;
-    marker.scale.y = 0.05;
-    marker.scale.z = 0.05;
+    marker.pose = mkr_pose;
+    marker.scale = mkr_size;
     marker.color.a = 1.0; // Don't forget to set the alpha!
     marker.color.r = 0.0;
     marker.color.g = 1.0;
@@ -54,14 +104,19 @@ visualization_msgs::Marker addMarker(const float& pt_x, const float& pt_y, const
 
 visualization_msgs::Marker addCentroid(const float& pt_x, const float& pt_y, const float& pt_z, const float& idx)
 {
-    return addMarker(pt_x, pt_y, pt_z, idx, "pcl_marker_centroid", visualization_msgs::Marker::SPHERE);
+    return addMarker(createPose(pt_x, pt_y, pt_z), createScale(0.05, 0.05, 0.05), idx, "pcl_marker_centroid", visualization_msgs::Marker::SPHERE);
+}
+
+visualization_msgs::Marker addBoundRect(const float& pt_x, const float& pt_y, const float& pt_z, const float& pt_l, const float& pt_b, const float& pt_h, const float& idx)
+{
+    return addMarker(createPose(pt_x, pt_y, pt_z), createScale(pt_l, pt_b, pt_h), idx, "pcl_marker_boundRect", visualization_msgs::Marker::CUBE);
 }
 
 //Downsample/Reduces num of pcl
-void vx_grid(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const float &leafSize)
+void vx_grid(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const float& leafSize)
 {
     //    ROS_INFO("Running Voxel Filter...");
-    constexpr float f_limit[2] = { 0.01f, 1.8f };
+    constexpr float f_limit[2] = { 0.01f, 3.0f };
     pcl::VoxelGrid<pcl::PointXYZRGBA> vxg;
 
     vxg.setInputCloud(cld_in.makeShared());
@@ -74,7 +129,7 @@ void vx_grid(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<p
 }
 
 //Filters noise
-void sor_filter(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const int &mean_k, const float &std_thres)
+void sor_filter(const pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::PointXYZRGBA>& cld_out, const int& mean_k, const float& std_thres)
 {
     pcl::StatisticalOutlierRemoval<pcl::PointXYZRGBA> sor;
 
@@ -131,6 +186,71 @@ void p_seg(pcl::PointCloud<pcl::PointXYZRGBA>& cld_in, pcl::PointCloud<pcl::Poin
     }
 }
 
+void euclus(pcl::PointCloud<pcl::PointXYZRGBA>& cld_in)
+{
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
+    tree->setInputCloud(cld_in.makeShared());
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZRGBA> ec;
+    ec.setClusterTolerance(0.02); // 2cm
+    ec.setMinClusterSize(150);
+    ec.setMaxClusterSize(25000);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cld_in.makeShared());
+    ec.extract(cluster_indices);
+
+    ROS_INFO("Clusters: %i", cluster_indices.size());
+
+    //Markers
+    visualization_msgs::MarkerArray m_centroid;
+    visualization_msgs::MarkerArray m_boundRect;
+
+    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it) {
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZRGBA>);
+
+        minMax bound_points; //Used to keep track of the bounding volume of the pcl cluster
+        initMinMax(bound_points);
+
+        for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
+            cloud_cluster->push_back((cld_in)[*pit]); //*
+            //ROS_INFO("XX [%i]: %i",j, *pit);
+            cld_in.points[*pit].r = 0;
+            cld_in.points[*pit].g = 40 * (j + 1);
+            cld_in.points[*pit].b = 0;
+            cld_in.points[*pit].a = 255;
+            compareVals(cld_in.points[*pit], bound_points); //Get minMax vals of cluster
+            //ROS_INFO("MinMax [x y z x y z]: %f %f %f %f %f %f", bound_points.pt_min.x,bound_points.pt_min.y,bound_points.pt_min.z, bound_points.pt_max.x,bound_points.pt_max.y,bound_points.pt_max.z);
+        }
+        ROS_INFO("Final MinMax [x y z x y z]: %f %f %f %f %f %f", bound_points.pt_min.x, bound_points.pt_min.y, bound_points.pt_min.z, bound_points.pt_max.x, bound_points.pt_max.y, bound_points.pt_max.z);
+
+        cloud_cluster->width = cloud_cluster->size();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+        ROS_INFO("Cluster [%i]: %i", j, cloud_cluster->size());
+
+        //Calculate centroid
+        Eigen::Vector4f centroid;
+        pcl::compute3DCentroid(*cloud_cluster, centroid);
+        ROS_INFO("Centroid [%i]: %f %f %f %f", j, centroid[0], centroid[1], centroid[2], centroid[3]);
+        m_centroid.markers.push_back(addCentroid(centroid[0], centroid[1], centroid[2], j));
+
+        float length{ abs(bound_points.pt_min.x) + bound_points.pt_max.x };
+        float breadth{ abs(bound_points.pt_min.y) + bound_points.pt_max.y };
+        float height{ abs(bound_points.pt_min.z) + bound_points.pt_max.z };
+        m_boundRect.markers.push_back(addBoundRect(centroid[0], centroid[1], centroid[2], length, breadth, height, j));
+        ROS_INFO("Final lbh [x y z]: %f %f %f ", length, breadth, height);
+        pubObjSeg.publish(cld_in);
+
+        ++j;
+    }
+    pcl::io::savePCDFileASCII("save_3.pcd", cld_in);
+    pubMarker.publish(m_centroid);
+    pubBoundRect.publish(m_boundRect);
+}
+
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& pcl_in)
 {
     // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
@@ -145,56 +265,10 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& pcl_in)
         cloud.points[i].a = 255;
     }
 
-    vx_grid(cloud, cloud_filtered,  0.005f); 		 //Voxel filter, 0.5cm leafsize
+    vx_grid(cloud, cloud_filtered, 0.005f); //Voxel filter, 0.5cm leafsize
     sor_filter(cloud_filtered, cloud_filtered2, 50, 0.15f); //StatisticalOutlierRemoval Filter, mean_k:50, std_dv_threshold: 0.15f
-    //p_seg(cloud_filtered2,cloud_filtered3);	 //Segmentation
-
-    // Creating the KdTree object for the search method of the extraction
-    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGBA>);
-    tree->setInputCloud(cloud_filtered2.makeShared());
-
-    std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZRGBA> ec;
-    ec.setClusterTolerance(0.02); // 2cm
-    ec.setMinClusterSize(150);
-    ec.setMaxClusterSize(25000);
-    ec.setSearchMethod(tree);
-    ec.setInputCloud(cloud_filtered2.makeShared());
-    ec.extract(cluster_indices);
-
-    ROS_INFO("Clusters: %i", cluster_indices.size());
-
-    visualization_msgs::MarkerArray m_a;
-
-    int j = 0;
-    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it) {
-        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZRGBA>);
-        for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
-            cloud_cluster->push_back((cloud_filtered2)[*pit]); //*
-            //ROS_INFO("XX [%i]: %i",j, *pit);
-            cloud_filtered.points[*pit].r = 0;
-            cloud_filtered2.points[*pit].g = 40 * (j);
-            cloud_filtered2.points[*pit].b = 0;
-            cloud_filtered2.points[*pit].a = 255;
-        }
-        cloud_cluster->width = cloud_cluster->size();
-        cloud_cluster->height = 1;
-        cloud_cluster->is_dense = true;
-        ROS_INFO("Cluster [%i]: %i", j, cloud_cluster->size());
-
-        //Calculate centroid
-        Eigen::Vector4f centroid;
-        pcl::compute3DCentroid(*cloud_cluster, centroid);
-        ROS_INFO("Centroid [%i]: %f %f %f %f", j, centroid[0], centroid[1], centroid[2], centroid[3]);
-
-        m_a.markers.push_back(addCentroid(centroid[0], centroid[1], centroid[2], j));
-
-        pubObjSeg.publish(cloud_filtered2);
-
-        ++j;
-    }
-    pcl::io::savePCDFileASCII("save_3.pcd", cloud_filtered2);
-    pubMarker.publish(m_a);
+    //p_seg(cloud_filtered2,cloud_filtered3);	 //Segmentation 	//Cpommented out as stereo image does not show any ground plane
+    euclus(cloud_filtered2);
 }
 
 int main(int argc, char** argv)
@@ -205,12 +279,12 @@ int main(int argc, char** argv)
 
     //ros::param::set("dist_th", 0.1);	// Set ROS param
 
-    ros::Subscriber sub = nh.subscribe("/cam/points2", 1, cloud_cb);	// Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe("/cam/points2", 1, cloud_cb); // Create a ROS subscriber for the input point cloud
 
     // Create a ROS publisher for the model coefficients
     pubObjSeg = nh.advertise<pcl::PointCloud<pcl::PointXYZRGBA> >("obj_segmentation", 1);
     pubMarker = nh.advertise<visualization_msgs::MarkerArray>("obj_centroid", 1);
-
+    pubBoundRect = nh.advertise<visualization_msgs::MarkerArray>("obj_boundRect", 1);
     // Spin
     ros::spin();
 }
